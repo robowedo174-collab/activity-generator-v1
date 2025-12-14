081d1cbfeb518ba31b5d3d741affaeb64991ca5f
Иван Батурин <ivanbaturin@Mac.lan>
2025-12-14 23:00:12 +0300

    test: add test route to verify routing works

---
+++ app/test-route/page.tsx
@@ -0,0 +1,8 @@
+export default function TestRoute() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl">Тестовый маршрут работает!</h1>
+    </div>
+  );
+}
+
